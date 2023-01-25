ab336c9286013ff6020b92e617ea86237f06d18b
natefoxr <nate@foxr.xyz>
2023-01-24 17:58:53 -0800

    init

---
+++ app.js
@@ -0,0 +1,9 @@
+var options1 = {
+  width: 750,
+  height: 415,
+  channel: "co1azo",
+  parent: ["localhost"],
+  muted: true,
+  autoplay: true,
+};
+var player1 = new Twitch.Player("cole", options1);
