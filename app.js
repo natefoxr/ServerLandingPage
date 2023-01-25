2fd1f0514b9fce481513536cddb8332f8c2cd57e
natefoxr <nate@foxr.xyz>
2023-01-24 18:11:21 -0800

    Small asthetic changes

--- app.js
+++
@@ -1,9 +0,0 @@
-var options1 = {
-  width: 750,
-  height: 415,
-  channel: "co1azo",
-  parent: ["localhost"],
-  muted: true,
-  autoplay: true,
-};
-var player1 = new Twitch.Player("cole", options1);
